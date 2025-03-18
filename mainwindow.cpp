3c3500b5d5c6108f0b9b8495b55aafda093706a9
43HK <jh2005nj@outlook.com>
2025-03-18 20:44:09 +0800

    2.0

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,27 +4,24 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , image(std::make_unique<GraphicData>())
 {
     ui->setupUi(this);
 
-    alreadySave = false;
+    connect(ui->blurRadioButton,        SIGNAL(clicked()), this, SLOT(do_resetBlurMode()));
+    connect(ui->gaussianRadioButton,    SIGNAL(clicked()), this, SLOT(do_resetBlurMode()));
+    connect(ui->medianRadioButton,      SIGNAL(clicked()), this, SLOT(do_resetBlurMode()));
+    connect(ui->bilateralRadioButton,   SIGNAL(clicked()), this, SLOT(do_resetBlurMode()));
 
-    connect(ui->basicConfirmButton, SIGNAL(clicked()), this, SLOT(writeToImage()));
-    connect(ui->blurConfirmButton, SIGNAL(clicked()), this, SLOT(writeToImage()));
 
-    connect(ui->blurRadioButton, SIGNAL(clicked()), this, SLOT(do_resetBlurMode()));
-    connect(ui->gaussianRadioButton, SIGNAL(clicked()), this, SLOT(do_resetBlurMode()));
-    connect(ui->medianRadioButton, SIGNAL(clicked()), this, SLOT(do_resetBlurMode()));
-    connect(ui->bilateralRadioButton, SIGNAL(clicked()), this, SLOT(do_resetBlurMode()));
+    connect(ui->basicResetButton,       SIGNAL(clicked()), this, SLOT(do_resetBasicMode()));
+    connect(ui->blurResetButton,        SIGNAL(clicked()), this, SLOT(do_resetBlurMode()));
+    connect(ui->colorResetButton,       SIGNAL(clicked()), this, SLOT(do_resetColorMode()));
+    connect(ui->transformerResetButton, SIGNAL(clicked()), this, SLOT(do_resetTransformerMode()));
 
-    connect(ui->Tab, SIGNAL(currentChanged(int)), this, SLOT(on_Tab_Changed(int)));
-
-    connect(ui->basicResetButton, SIGNAL(clicked()), this, SLOT(do_resetBasicMode()));
-    connect(ui->blurResetButton, SIGNAL(clicked()), this, SLOT(do_resetBasicMode()));
-
-    connect(ui->actionLoad, SIGNAL(triggered()), this, SLOT(do_loadImage()));
-    connect(ui->actionSave, SIGNAL(triggered()), this, SLOT(do_saveImage()));
-    connect(ui->actionReset, SIGNAL(triggered()), this, SLOT(do_resetAll()));
+    connect(ui->actionLoad,     SIGNAL(triggered()), this, SLOT(do_loadImage()));
+    connect(ui->actionSave,     SIGNAL(triggered()), this, SLOT(do_saveImage()));
+    connect(ui->actionReset,    SIGNAL(triggered()), this, SLOT(do_resetAll()));
 
     if (cv::cuda::getCudaEnabledDeviceCount() > 0) qDebug() << "CUDA is supported!";
     else qDebug() << "CUDA is not supported or no device found.";
@@ -34,6 +31,10 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+// ------------------------------
+// 窗口的基础方法
+// ------------------------------
 
 // 重载窗口缩放的事件函数
 void MainWindow::resizeEvent(QResizeEvent *event)
@@ -60,33 +61,28 @@ void MainWindow::do_loadImage()
             }
 
             cvtColor(imageData, imageData, COLOR_BGR2RGB);
-            imageSrc = imageData.clone();
-            imageTemp = imageSrc.clone();
-
-            myImage = QImage((const unsigned char*)imageData.data, imageData.cols, imageData.rows, imageData.step, QImage::Format_RGB888).copy();
-            if(myImage.isNull())
-            {
-                qDebug() << "Failed to convert cv::Mat to QImage.";
-                return;
-            }
-
-            imageDisplay();
+            image->src = imageData.clone();
+            image->temp = imageData.clone();
+            image->dst = imageData.clone();
+            do_resetAll();
+            //imageDisplay();
             qDebug() << "Selected file path:" << imagePath;
         }
         else {qDebug() << "Failed to load image.";}
     }
-    //确保打开新文件的时候重置参数
-    do_resetContrast();
-    do_resetBrightness();
-    do_resetBlur();
 }
 
 void MainWindow::imageDisplay()
 {
+    myImage = QImage((const unsigned char*)(image->dst.data),
+                     image->dst.cols, image->dst.rows,
+                     image->dst.step,
+                     QImage::Format_RGB888).copy();
+
     // 获取 QLabel 当前尺寸
     QSize labelSize = ui->image->size();
 
-    // 原始图片的宽高比
+    // 原始图片的宽高比=
     qreal imageAspectRatio = myImage.width() / (qreal)myImage.height();
 
     // 计算缩放后的尺寸，保持宽高比
@@ -112,160 +108,303 @@ void MainWindow::imageDisplay()
     ui->image->setPixmap(scaledPixmap);
 }
 
+// ------------------------------
+// 图像处理方法
+// ------------------------------
 
-void MainWindow::basicImageProcess(int brightness, float contrast)
+void GraphicData::process(BlurMode nowMode)
 {
-    imageTemp.convertTo(imageDst, -1, contrast, brightness);
+    Mat tempBasic;
+    Mat tempColor;
+    Mat tempBlur;
 
-    myImage = QImage((const unsigned char*)(imageDst.data),
-                     imageDst.cols, imageDst.rows,
-                     imageDst.step,
-                     QImage::Format_RGB888).copy();
-
-    imageDisplay();
-}
-
-void MainWindow::blurImageProcess(int kernel)
-{
-    int kernelSize = (kernel % 2 == 1) ? kernel : kernel + 1;
-
-    switch(nowMode)
+    if (brightness != 0 || contrast != 1.0)
     {
-    case Mode::Blur:
-        cv::blur(imageTemp, imageDst, Size(kernelSize, kernelSize), Point(-1, -1));
-        break;
-
-    case Mode::GaussianBlur:
-        cv::GaussianBlur(imageTemp, imageDst, Size(kernelSize, kernelSize), 0, 0);
-        break;
-
-    case Mode::MedianBlur:
-        cv::medianBlur(imageTemp, imageDst, kernelSize);
-        break;
-
-    case Mode::BilateralFilter:
-        cv::bilateralFilter(imageTemp, imageDst, kernelSize, kernelSize * 2, kernelSize / 2);
-        break;
-
-    default:
-        break;
+        temp.convertTo(tempBasic, -1, contrast, brightness);
+    }
+    else
+    {
+        temp.copyTo(tempBasic);
     }
 
-    myImage = QImage((const unsigned char*)(imageDst.data),
-                     imageDst.cols, imageDst.rows,
-                     imageDst.step,
-                     QImage::Format_RGB888).copy();
+    if (H != 0 || S != 1.0 || V != 1.0)
+    {
+        adjustHSV(tempBasic).copyTo(tempColor);
+    }
+    else
+    {
+        tempBasic.copyTo(tempColor);
+    }
 
-    imageDisplay();
+    if (kernel != 1)
+    {
+        // 确保卷积核宽度为奇数
+        int kernelSize = (kernel % 2 == 1) ? kernel : kernel + 1;
+        switch(nowMode)
+        {
+        case BlurMode::Blur:
+            cv::blur(tempColor, tempBlur, Size(kernelSize, kernelSize), Point(-1, -1));
+            break;
+        case BlurMode::GaussianBlur:
+            cv::GaussianBlur(tempColor, tempBlur, Size(kernelSize, kernelSize), 0, 0);
+            break;
+        case BlurMode::MedianBlur:
+            cv::medianBlur(tempColor, tempBlur, kernelSize);
+            break;
+        case BlurMode::BilateralFilter:
+            cv::bilateralFilter(tempColor, tempBlur, kernelSize, kernelSize * 2, kernelSize / 2);
+            break;
+        case BlurMode::None:
+        default:
+            tempColor.copyTo(tempBlur);
+            break;
+        }
+
+    }
+    else
+    {tempColor.copyTo(tempBlur);}
+
+    if (angle != 0.0 || scale != 1.0)
+    {
+        Mat imageRot(2, 3, CV_32FC1);
+        // 计算原图片的中心点
+        Point centerPoint = Point(tempBlur.cols / 2, tempBlur.rows / 2);
+
+        imageRot = getRotationMatrix2D(centerPoint, angle, scale);
+
+        warpAffine(tempBlur, dst, imageRot, tempBlur.size());
+    }
+    else{tempBlur.copyTo(dst);}
 }
+
+Mat GraphicData::adjustHSV(Mat tempBasic)
+{
+    // 将图像从BGR颜色空间转换为HSV颜色空间
+    cv::cvtColor(tempBasic, tempBasic, cv::COLOR_RGB2HSV);
+
+    // 分离通道
+    std::vector<cv::Mat> channels;
+    cv::split(tempBasic, channels);
+
+    cv::Mat hue = channels[0];
+    cv::Mat saturation = channels[1];
+    cv::Mat value = channels[2];
+
+    // 调整色调（hue），注意处理溢出
+    if (H != 0)
+    {
+        hue += H;
+        // 处理溢出: HSV中Hue范围是0-180在OpenCV中
+        hue.setTo(0, hue < 0);
+        hue.setTo(180, hue > 180);
+    }
+
+    // 调整饱和度（saturation）和亮度（value）
+    if (S != 1.0)
+    {
+        saturation.convertTo(saturation, -1, S, 0);
+        // 确保像素值在0到255之间
+        cv::threshold(saturation, saturation, 255, 255, cv::THRESH_TRUNC);
+        cv::threshold(saturation, saturation, 0, 0, cv::THRESH_TOZERO);
+    }
+    if (V != 1.0)
+    {
+        value.convertTo(value, -1, V, 0);
+        // 确保像素值在0到255之间
+        cv::threshold(value, value, 255, 255, cv::THRESH_TRUNC);
+        cv::threshold(value, value, 0, 0, cv::THRESH_TOZERO);
+    }
+
+    // 合并通道
+    std::vector<cv::Mat> adjustedChannels = {hue, saturation, value};
+    cv::merge(adjustedChannels, tempBasic);
+
+    Mat tempRGB;
+    cv::cvtColor(tempBasic, tempRGB, cv::COLOR_HSV2RGB);
+
+    return tempRGB;
+}
+
+// ------------------------------
+// 控件响应事件
+// ------------------------------
 
 void MainWindow::on_brightnessSlider_sliderMoved(int position)
 {
-    if (!alreadySave) do_resetBlurMode();
-    nowMode = Mode::Basic;
-    basicImageProcess(position, 1.0);
-    alreadySave = false;
+    image->brightness = position;
+    image->process(nowMode);
+    imageDisplay();
+}
+void MainWindow::on_brightnessSlider_valueChanged(int value)
+{
+    image->brightness = value;
+    image->process(nowMode);
+    imageDisplay();
 }
 void MainWindow::on_contrastSlider_sliderMoved(int position)
 {
-    if (!alreadySave) do_resetBlurMode();
-    nowMode = Mode::Basic;
-    basicImageProcess(0, position / 33.3);
-    alreadySave = false;
+    image->contrast = position / 33.0;
+    image->process(nowMode);
+    imageDisplay();
+}
+void MainWindow::on_contrastSlider_valueChanged(int value)
+{
+    image->contrast = value / 33.0;
+    image->process(nowMode);
+    imageDisplay();
 }
 void MainWindow::on_blurSlider_sliderMoved(int position)
 {
-    if (!alreadySave) do_resetBasicMode();
+    if      (ui->blurRadioButton->isChecked())      nowMode = BlurMode::Blur;
+    else if (ui->gaussianRadioButton->isChecked())  nowMode = BlurMode::GaussianBlur;
+    else if (ui->medianRadioButton->isChecked())    nowMode = BlurMode::MedianBlur;
+    else if (ui->bilateralRadioButton->isChecked()) nowMode = BlurMode::BilateralFilter;
 
-    if      (ui->blurRadioButton->isChecked())      nowMode = Mode::Blur;
-    else if (ui->gaussianRadioButton->isChecked())  nowMode = Mode::GaussianBlur;
-    else if (ui->medianRadioButton->isChecked())    nowMode = Mode::MedianBlur;
-    else if (ui->bilateralRadioButton->isChecked()) nowMode = Mode::BilateralFilter;
-
-    blurImageProcess(position);
-    alreadySave = false;
+    image->kernel = position;
+    image->process(nowMode);
+    imageDisplay();
 }
-
-
-void MainWindow::writeToImage()
+void MainWindow::on_blurSlider_valueChanged(int value)
 {
-    alreadySave = true;
+    if      (ui->blurRadioButton->isChecked())      nowMode = BlurMode::Blur;
+    else if (ui->gaussianRadioButton->isChecked())  nowMode = BlurMode::GaussianBlur;
+    else if (ui->medianRadioButton->isChecked())    nowMode = BlurMode::MedianBlur;
+    else if (ui->bilateralRadioButton->isChecked()) nowMode = BlurMode::BilateralFilter;
 
-    switch(nowMode)
-    {
-    case Mode::Basic:
-        do_resetBrightness();
-        do_resetContrast();
-        imageDst.copyTo(imageTemp);
-        break;
-    case Mode::Blur:
-        ui->blurRadioButton->setChecked(false);
-        do_resetBlur();
-        imageDst.copyTo(imageTemp);
-        break;
-    case Mode::GaussianBlur:
-        ui->gaussianRadioButton->setChecked(false);
-        do_resetBlur();
-        imageDst.copyTo(imageTemp);
-        break;
-    case Mode::MedianBlur:
-        ui->medianRadioButton->setChecked(false);
-        do_resetBlur();
-        imageDst.copyTo(imageTemp);
-        break;
-    case Mode::BilateralFilter:
-        ui->bilateralRadioButton->setChecked(false);
-        do_resetBlur();
-        imageDst.copyTo(imageTemp);
-        break;
-    default:
-        break;
-    }
+    image->kernel = value;
+    image->process(nowMode);
+    imageDisplay();
+}
+void MainWindow::on_HSlider_sliderMoved(int position)
+{
+    image->H = position;
+    image->process(nowMode);
+    imageDisplay();
+}
+void MainWindow::on_HSlider_valueChanged(int value)
+{
+    image->H = value;
+    image->process(nowMode);
+    imageDisplay();
+}
+void MainWindow::on_SSlider_sliderMoved(int position)
+{
+    image->S = position / 33.0;
+    image->process(nowMode);
+    imageDisplay();
+}
+void MainWindow::on_SSlider_valueChanged(int value)
+{
+    image->S = value / 33.0;
+    image->process(nowMode);
+    imageDisplay();
+}
+void MainWindow::on_VSlider_sliderMoved(int position)
+{
+    image->V = position / 33.0;
+    image->process(nowMode);
+    imageDisplay();
+}
+void MainWindow::on_VSlider_valueChanged(int value)
+{
+    image->V = value / 33.0;
+    image->process(nowMode);
+    imageDisplay();
 }
 
+void MainWindow::on_minusButton_clicked()
+{
+    image->scale -= 0.1;
+    image->process(nowMode);
+    imageDisplay();
+}
+void MainWindow::on_plusButton_clicked()
+{
+    image->scale += 0.1;
+    image->process(nowMode);
+    imageDisplay();
+}
+void MainWindow::on_leftRotButton_clicked()
+{
+    image->angle += 5.0;
+    image->process(nowMode);
+    imageDisplay();
+}
+void MainWindow::on_rightRotButton_clicked()
+{
+    image->angle -= 5.0;
+    image->process(nowMode);
+    imageDisplay();
+}
 
-void MainWindow::do_resetBrightness()
-{ui->brightnessSlider->setValue(0);}
-void MainWindow::do_resetContrast()
-{ui->contrastSlider->setValue(33);}
-void MainWindow::do_resetBlur()
-{ui->blurSlider->setValue(1);}
+// ------------------------------
+// 重置函数
+// ------------------------------
 
 void MainWindow::do_resetAll()
 {
-    do_resetBrightness();
-    do_resetContrast();
-    do_resetBlur();
-    imageDst = imageSrc.clone();
-    myImage = QImage((const unsigned char*)(imageDst.data),
-                     imageDst.cols, imageDst.rows,
-                     imageDst.step,
-                     QImage::Format_RGB888).copy();
+    ui->brightnessSlider->setValue(0);
+    image->brightness = 0;
+    ui->contrastSlider->setValue(33);
+    image->contrast = 1.0;
+    ui->blurSlider->setValue(1);
+    image->kernel = 1;
+    ui->HSlider->setValue(0);
+    image->H = 0;
+    ui->SSlider->setValue(33);
+    image->S = 1.0;
+    ui->VSlider->setValue(33);
+    image->V = 1.0;
+
+    image->angle = 0.0;
+    image->scale = 1.0;
+
+    image->process(BlurMode::None);
     imageDisplay();
 }
 
 void MainWindow::do_resetBasicMode()
 {
-    do_resetBrightness();
-    do_resetContrast();
-    imageDst = imageTemp.clone();
-    myImage = QImage((const unsigned char*)(imageDst.data),
-                     imageDst.cols, imageDst.rows,
-                     imageDst.step,
-                     QImage::Format_RGB888).copy();
+    ui->brightnessSlider->setValue(0);
+    image->brightness = 0;
+    ui->contrastSlider->setValue(33);
+    image->contrast = 1.0;
+
+    image->process(nowMode);
+    imageDisplay();
+}
+void MainWindow::do_resetBlurMode()
+{
+    ui->blurSlider->setValue(1);
+    image->kernel = 1;
+
+    image->process(BlurMode::None);
+    imageDisplay();
+}
+void MainWindow::do_resetColorMode()
+{
+    ui->HSlider->setValue(0);
+    image->H = 0;
+    ui->SSlider->setValue(33);
+    image->S = 1.0;
+    ui->VSlider->setValue(33);
+    image->V = 1.0;
+
+    image->process(nowMode);
+    imageDisplay();
+}
+void MainWindow::do_resetTransformerMode()
+{
+    image->angle = 0.0;
+    image->scale = 1.0;
+
+    image->process(nowMode);
     imageDisplay();
 }
 
-void MainWindow::do_resetBlurMode()
-{
-    do_resetBlur();
-    imageDst = imageTemp.clone();
-    myImage = QImage((const unsigned char*)(imageDst.data),
-                     imageDst.cols, imageDst.rows,
-                     imageDst.step,
-                     QImage::Format_RGB888).copy();
-    imageDisplay();
-}
+// ------------------------------
+// 图片保存方法
+// ------------------------------
 
 void MainWindow::do_saveImage()
 {
--- mainwindow.h
+++ mainwindow.h
@@ -6,6 +6,8 @@
 #include <QScreen>
 #include <QMessageBox>
 
+#include <memory>
+
 #include "opencv2/opencv.hpp"
 using namespace cv;
 
@@ -15,14 +17,45 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-enum class Mode
+enum class BlurMode
 {
-    Basic,
     Blur,
     GaussianBlur,
     MedianBlur,
     BilateralFilter,
     None
+};
+
+class GraphicData
+{
+public:
+    GraphicData() :
+        brightness(0),
+        contrast(1.0f),
+        kernel(1),
+        H(0),
+        S(1.0f),
+        V(1.0f),
+        angle(0.0f),
+        scale(1.0f){}
+    ~GraphicData(){}
+
+    Mat adjustHSV(Mat temp);
+    void process(BlurMode nowMode);
+    void resetAll();
+
+    int brightness;
+    float contrast;
+    int kernel;
+    int H;
+    float S;
+    float V;
+    float angle;
+    float scale;
+
+    Mat src;
+    Mat temp;
+    Mat dst;
 };
 
 class MainWindow : public QMainWindow
@@ -35,38 +68,44 @@ public:
 
     void resizeEvent(QResizeEvent *event);
 
-    void basicImageProcess(int brightness, float contrast);
-    void blurImageProcess(int kernel);
-
     void imageDisplay();
 
 private slots:
     void do_loadImage();
 
     void on_contrastSlider_sliderMoved(int position);
+    void on_contrastSlider_valueChanged(int value);
     void on_brightnessSlider_sliderMoved(int position);
+    void on_brightnessSlider_valueChanged(int value);
     void on_blurSlider_sliderMoved(int position);
+    void on_blurSlider_valueChanged(int value);
 
-    void writeToImage();
+    void on_HSlider_sliderMoved(int position);
+    void on_HSlider_valueChanged(int value);
+    void on_SSlider_sliderMoved(int position);
+    void on_SSlider_valueChanged(int value);
+    void on_VSlider_sliderMoved(int position);
+    void on_VSlider_valueChanged(int value);
 
-    void do_resetContrast();
-    void do_resetBrightness();
-    void do_resetBlur();
+    void on_minusButton_clicked();
+    void on_plusButton_clicked();
+    void on_leftRotButton_clicked();
+    void on_rightRotButton_clicked();
+
 
     void do_resetAll();
     void do_resetBasicMode();
     void do_resetBlurMode();
+    void do_resetColorMode();
+    void do_resetTransformerMode();
 
     void do_saveImage();
 private:
     Ui::MainWindow *ui;
+    std::unique_ptr<GraphicData> image;
 
-    Mode nowMode;
-    bool alreadySave;
+    BlurMode nowMode = BlurMode::None;
 
-    Mat imageSrc;
-    Mat imageTemp;
-    Mat imageDst;
     QImage myImage;
 
 };
